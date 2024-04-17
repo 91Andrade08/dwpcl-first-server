309d90f2901b67e38f9d3665969f92faa1df2ad4
91Andrade08 <102134857+91Andrade08@users.noreply.github.com>
2024-04-17 20:51:56 +0000

    🚧Procesando Formulario

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import http from "http";
 import path from "path";
 import { promises as fs } from 'fs';
-
 global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
-
 const server = http.createServer(async (req, res) => {
   // Desestructurando de "req"
   let { url, method } = req;
-
   console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
-
   // Enrutando peticiones
   switch (url) {
     case '/':
@@ -36,19 +32,20 @@ const server = http.createServer(async (req, res) => {
       res.end();
       break;
     case '/author':
-      
+      // Peticion raiz
       // Estableciendo cabeceras
       res.setHeader('Content-Type', 'text/html');
-      
+      let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
+      // Escribiendo la respuesta
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/x-icon" sizes="16x16" href="/favicon.ico">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
         </head>
         <body style="text-align: center;">
           <h1 style="color: #333;">&#9889; Author &#9889;</h1>
-          <p style="color: #34495E;">Juan Manuel González</p>
+          <p style="color: #34495E;">Juan Manuel Gonzalez Student</p>
           <div>
             <img width="300px" src="https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=" alt="Foto Ivan Rivalcoba">
           </div>
@@ -64,11 +61,11 @@ const server = http.createServer(async (req, res) => {
     case "/favicon.ico":
       // Especificar la ubicación del archivo de icono
       const faviconPath = path.join(__dirname, 'favicon.ico');
-      try{
+      try {
         const data = await fs.readFile(faviconPath);
-        res.writeHead(200, {'Content-Type': 'image/x-icon'});
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' });
         res.end(data);
-      }catch (err) {
+      } catch (err) {
         console.error(err);
         // Peticion raiz
         // Estableciendo cabeceras
@@ -94,7 +91,7 @@ const server = http.createServer(async (req, res) => {
         res.end();
       }
       break;
-      case "/message":
+    case "/message":
       // Verificando si es post
       if (method === "POST") {
         // Procesa el formulario
@@ -106,6 +103,7 @@ const server = http.createServer(async (req, res) => {
       }
       res.end();
       break;
+      // Continua con el defautl
     default:
       // Peticion raiz
       // Estableciendo cabeceras
@@ -131,7 +129,6 @@ const server = http.createServer(async (req, res) => {
       break;
   }
 }); 
-
 server.listen(3000, "0.0.0.0", () => {
   console.log("👩‍🍳 Servidor escuchando en http://localhost:3000"); 
 });
